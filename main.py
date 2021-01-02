82c74ebe7ac6849bc4832ed0a08b54c9f92c8f3d
zascerta <giammarco.daless@gmail.com>
2021-01-02 13:43:10 +0100

    format prints

--- main.py
+++ main.py
@@ -5,17 +5,18 @@ from scot.var import VAR
 from scot.connectivity import Connectivity
 from scot.plotting import plot_connectivity_spectrum
 
+
 file_name = "data/S003R01.edf"
 data = mne.io.read_raw_edf(file_name, verbose=True)
 
-# you can get the metadata included in the file_name and a list of all channels:
+# get the metadata included in the file_name and a list of all channels:
 info = data.info
 channels = data.ch_names
 print("\n",info)
 print("\n",channels)
 
 raw_data = data.get_data()
-print("raw data shape :", raw_data.shape)
+print("raw_data shape:", raw_data.shape)
 
 """
 #data.plot_psd(fmax=50)
@@ -24,7 +25,7 @@ print("raw data shape :", raw_data.shape)
 
 var = VAR(model_order=40)
 fitted = var.fit(raw_data)
-print("VAR coefficient:",fitted.coef.shape)
+print("\nVAR coefficient:",fitted.coef.shape)
 
 conn = Connectivity(fitted.coef)
 pdc  = conn.PDC()
