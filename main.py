b8a32cfb23f8569608f55ead1d25db40f6f0ccac
zascerta <giammarco.daless@gmail.com>
2021-01-02 12:19:38 +0100

    changed pip requirements, using SCoT

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 import mne
-from pdc_tdf import *
+import scot.connectivity
 
-file_name = "S003R01.edf"
+file_name = "data/S003R01.edf"
 data = mne.io.read_raw_edf(file_name, verbose=True)
 
 # you can get the metadata included in the file_name and a list of all channels:
-raw_data = data.get_data()
 info = data.info
 channels = data.ch_names
 print("\n",info)
 print("\n",channels)
 
-#data.plot_psd(fmax=50)
-data.plot(start=0,duration=5, n_channels=30, verbose=True)
+raw_data = data.get_data()
+print("raw data shape:", raw_data.shape)
+
 
 """
+#data.plot_psd(fmax=50)
+#data.plot(start=0,duration=5, n_channels=30, verbose=True)
+
 #events = mne.read_events(file_name + ".event")
 #print(events)
 """
