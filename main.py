5f9213461c2eb6b55f2a67ab9cb86017c4f381ab
zascerta <giammarco.daless@gmail.com>
2021-01-02 12:45:25 +0100

    testing var analysis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import mne
-import scot.connectivity
+from scot.var import VAR
+from scot.connectivity import Connectivity
 
 file_name = "data/S003R01.edf"
 data = mne.io.read_raw_edf(file_name, verbose=True)
@@ -11,13 +12,15 @@ print("\n",info)
 print("\n",channels)
 
 raw_data = data.get_data()
-print("raw data shape:", raw_data.shape)
-
+print("raw data shape :", raw_data.shape)
 
 """
 #data.plot_psd(fmax=50)
 #data.plot(start=0,duration=5, n_channels=30, verbose=True)
-
-#events = mne.read_events(file_name + ".event")
-#print(events)
 """
+
+var = VAR(model_order=40)
+fitted = var.fit(raw_data)
+print("VAR coefficient:",fitted.coef.shape)
+
+conn = Connectivity(fitted.coef)
