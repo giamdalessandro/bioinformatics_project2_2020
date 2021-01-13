1143f880ed4901cd94aad5e1282bb9c88222d312
balthier <luca799791@gmail.com>
2021-01-13 17:30:09 +0100

    p1.4 - lib is working

--- community_detection.py
+++ community_detection.py
@@ -1,22 +1,31 @@
-import community as community_louvain
+import community as cl
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 import networkx as nx
 
 # load the karate club graph
-G = nx.karate_club_graph()
+G = nx.random_lobster(100, 0.9, 0.9)
+print("Graph has {} nodes and {} edges".format(len(G.nodes()), len(G.edges())))
 
-#first compute the best partition
-partition = community_louvain.best_partition(G)
+dendo = cl.generate_dendrogram(G)
+print("len dendogram", len(dendo))
+for level in range(len(dendo)-1) :
+    partition = cl.partition_at_level(dendo, level)
+    pos = nx.circular_layout(G)
+    # color the nodes according to their partition
+    cmap = cm.get_cmap('viridis', max(partition.values()) + 1)
+    nx.draw_networkx_nodes(G, pos, partition.keys(), node_size=40, cmap=cmap, node_color=list(partition.values()))
+    nx.draw_networkx_edges(G, pos, alpha=0.5)
+    plt.show()
+    print("modularity = ", cl.modularity(partition, G))
 
 # compute the best partition
-partition = community_louvain.best_partition(G)
-
-# draw the graph
-pos = nx.spring_layout(G)
+partition = cl.best_partition(G)
+pos = nx.circular_layout(G)
 # color the nodes according to their partition
 cmap = cm.get_cmap('viridis', max(partition.values()) + 1)
-nx.draw_networkx_nodes(G, pos, partition.keys(), node_size=40,
-                       cmap=cmap, node_color=list(partition.values()))
+nx.draw_networkx_nodes(G, pos, partition.keys(
+), node_size=40, cmap=cmap, node_color=list(partition.values()))
 nx.draw_networkx_edges(G, pos, alpha=0.5)
 plt.show()
+print("best modularity = ", cl.modularity(partition, G))
